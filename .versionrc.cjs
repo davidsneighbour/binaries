72b50bdff9966302540d2086b9285854afa530f4
Patrick Kollitsch <patrick@davids-neighbour.com>
2024-11-23 07:45:25 +0700

    build(fix): update release config and cleanup
    Signed-off-by: Patrick Kollitsch <patrick@davids-neighbour.com>

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,12 +1,5 @@
 const defaultStandardVersion = require("@davidsneighbour/release-config");
-
 const localStandardVersion = {
-  scripts: {
-    prerelease: "",
-  },
-  skip: {
-    changelog: true
-  }
 };
 
 const standardVersion = {
